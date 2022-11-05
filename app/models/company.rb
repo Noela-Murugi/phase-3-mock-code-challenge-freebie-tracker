08f9e5c37679139199116596d669180e684c3b6b
Noela-Murugi <murugin254@gmail.com>
2022-11-05 17:02:40 +0300

    completed the assignment

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,12 @@
 class Company < ActiveRecord::Base
+  has_many :freebies
+  has_many :devs, through: :freebies
 
+  def give_freebie(dev, item_name, value)
+    self.freebies << Freebie.create(dev: dev, item_name: item_name, value: value)
+  end
+
+  def self.oldest_company
+    Company.find_by founding_year: self.minimum(:founding_year)
+  end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,14 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def received_one?(item_name)
+    self.freebies.find_by item_name: item_name ? return true : return false
+    end
+  end
+
+  def give_away(dev, freebie)
+     self.received_one? freebie.item_name
+      freebie.dev = dev
+  end
 end
